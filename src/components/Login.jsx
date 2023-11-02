aa83deb690d90e583a94cb9ed7d0c43adc930ba4
LaveMando <104777054+LaveMando@users.noreply.github.com>
2023-11-02 09:57:50 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,25 +5,36 @@ import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
-    const [values, setValues] = useState({
-        email: '',
-        password: ''
-    })
+    // const [values, setValues] = useState({
+    //     email: '',
+    //     password: ''
+    // })
+
+    const [email,setEmail] = useState("");
+    const [password, setPassowrd] = useState("");
+
     const [error, setError] = useState(null)
     const navigate = useNavigate()
     axios.defaults.withCredentials = true;
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        axios.post('http://localhost:3000/auth/adminlogin', values)
-        .then(result => {
-            if(result.data.loginStatus) {
-                localStorage.setItem("valid", true)
-                navigate('/dashboard')
-            } else {
-                setError(result.data.Error)
+        axios
+          .post("http://localhost:8800/admin", { email, password })
+          .then((res) => {
+            console.log(res);
+            if(res.data === "Login Successful"){
+                navigate("/dashboard")
             }
-        })
-        .catch(err => console.log(err))
+            else{
+             console.log("Not working")   
+            }
+            // navigate("/dashoboard");
+          })
+          .catch((err) => {
+            console.log(err);
+            // navigate("/");
+            // Handle the error here, such as displaying an error message to the user
+          });
     }
 
   return (
@@ -36,13 +47,13 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor="email"><strong>Email:</strong></label>
-                    <input type="email" name='email' autoComplete='off' placeholder='Enter Email'
-                     onChange={(e) => setValues({...values, email : e.target.value})} className='form-control rounded-0'/>
+                    <input  name='email' autoComplete='off' placeholder='Enter Email'
+                     onChange={(e) => setEmail(e.target.value)} className='form-control rounded-0'/>
                 </div>
                 <div className='mb-3'> 
                     <label htmlFor="password"><strong>Password:</strong></label>
                     <input type="password" name='password' placeholder='Enter Password'
-                     onChange={(e) => setValues({...values, password : e.target.value})} className='form-control rounded-0'/>
+                     onChange={(e) => setPassowrd(e.target.value)} className='form-control rounded-0'/>
                 </div>
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
                 <div className='mb-1'> 
