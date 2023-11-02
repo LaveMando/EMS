9d749b90b769df43c18b521b5ad158af85259c99
LaveMando <104777054+LaveMando@users.noreply.github.com>
2023-11-02 10:00:39 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,64 +1,117 @@
-import express from 'express';
-import cors from 'cors';
-import { config } from 'dotenv'; // Import config from dotenv
 import mysql from 'mysql';
-import cookieParser from 'cookie-parser';
-import Jwt from 'jsonwebtoken';
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
 
-config(); // Initialize dotenv
-
-const app = express();
+const app = express()
+app.use(bodyParser.json());
+app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: "http://localhost:5173",
+    methods:["POST","GET","DELETE","PUT"],
     credentials: true
-}));
-app.use(express.json());
-app.use(cookieParser());
+}))
 
-// Move the middleware that requires the body parsing above other routes
-app.use(express.static('Public'));
-
-const jwtSecretKey = process.env.JWT_SECRET_KEY; // Use environment variable for JWT secret
-const dbPassword = process.env.DB_PASSWORD; // Use environment variable for database password
-
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Sagehill#23',
-    database: 'authusers'
-});
+    database: 'employee_management'
+})
 
-// Connect to the database
-connection.connect(function (error) {
-    if (error) throw error;
-    else console.log('Successful connection');
-});
+app.get('/admin', (req, res)=> {
+    const q = "SELECT * FROM admin"
+    con.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+} )
 
-// Handle errors in JWT verification
-const handleJWTError = (res, error) => {
-    console.error('JWT Verification Error: ', error);
-    return res.status(401).json({ Status: false, Error: 'Unauthorized' });
-};
+app.get('/employeelog', (req, res)=> {
+    const q = "SELECT * FROM employeelog"
+    con.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+} )
 
-const verifyUser = (req, res, next) => {
-    const token = req.cookies.token;
-    if (token) {
-        Jwt.verify(token, jwtSecretKey, (err, decoded) => {
-            if (err) return handleJWTError(res, err);
-            req.id = decoded.id;
-            req.role = decoded.role;
-            next();
-        });
+app.post('/send_email', (req, res) => {
+    const formData = req.body;
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'ltmandoza@gmail.com', // your email
+        pass: 'qmduhpnfttngghmy' // your email password
+      }
+    });
+  
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"Leave Application" <ltmandoza@gmail.com>', // sender address
+      to: 'lavemando@gmail.com', // list of receivers
+      subject: 'New Leave Application', // Subject line
+      text: `Employee Name: ${formData.employeeName}
+             Start Date of Leave: ${formData.startDate}
+             End Date of Leave: ${formData.endDate}
+             Leave Category: ${formData.leaveCategory}
+             Additional Explanation: ${formData.additionalExplanation}`, // plain text body
+    };
+  
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+    });
+  
+    res.json({ status: 'Email sent' });
+  });
+  
+
+// REQUEST
+// app.get("/",(res,req) =>{
+//     res.json("Hello this is the backend")
+// })
+app.listen(8800, ()=>{
+    console.log("Connected, Ndakaipa")
+})
+
+con.connect(function(err){
+    if(err) {
+        console.log("connection error")
     } else {
-        return res.status(401).json({ Status: false, Error: 'Not authenticated' });
+        console.log("Connected")
     }
-};
+})
 
-app.get('/verify', verifyUser, (req, res) => {
-    return res.json({ Status: true, role: req.role, id: req.id });
-});
+// ADMINISTRATOR LOGIN
+app.post('/admin', (req, res)=> {
+    const login = "SELECT * FROM admin WHERE email=? AND password=?";
 
-app.listen(3000, () => {
-    console.log('Server is running');
-});
+    con.query(login, [req.body.email, req.body.password], (err,data)=>{
+        if(err) return res.json("Error")
+        if(data.length > 0){
+            return res.json("Login Successful")
+        }else {
+            return res.json("Login Failed")
+        }
+    })
+})
+
+// EMPLOYEE LOGIN
+app.post('/employeelog', (req, res)=> {
+    const login = "SELECT * FROM employeelog WHERE email=? AND password=?";
+
+    con.query(login, [req.body.email, req.body.password], (err,data)=>{
+        if(err) return res.json("Error")
+        if(data.length > 0){
+            return res.json("Login Successful")
+        }else {
+            return res.json("Login Failed")
+        }
+    })
+})
